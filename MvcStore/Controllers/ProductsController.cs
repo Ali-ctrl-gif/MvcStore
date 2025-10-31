e02c065b261efe7fb77660db8cac95af5ec44998
AliAsghar <mirpasha8181@gmail.com>
2025-10-31 21:47:52 +0330

    Add project files.

---
+++ MvcStore/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcStore.Models;
+
+namespace MvcStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static List<Product> _products = new List<Product>()
+        {
+            new Product {Id = 1, Name = "Laptop" , Type = "Electronics" , Price = 80000000M,Quantity = 10},
+            new Product {Id = 2, Name = "AirPad" , Type = "Electronics" , Price = 10000000M,Quantity = 15},
+            new Product {Id = 3, Name = "Desk" , Type = "Furniture" , Price = 2000000M , Quantity = 20}
+        };
+        public ActionResult Index()
+        {
+            return View(_products);
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var product = _products.Find(b => b.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Product product)
+        {
+            product.Id = _products.Count + 1;
+            _products.Add(product);
+            return RedirectToAction("Index");
+        }
+    }   
+}
---
+++ MvcStore/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity {  get; set; }
+    }
+}
