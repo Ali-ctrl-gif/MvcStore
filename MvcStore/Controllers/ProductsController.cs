31c1164172a27c71c642bb61d01bc435e945834b
AliAsghar <mirpasha8181@gmail.com>
2025-11-02 14:13:06 +0330

    Add Edit & Delete

--- MvcStore/Controllers/ProductsController.cs
+++ MvcStore/Controllers/ProductsController.cs
@@ -35,7 +35,52 @@ namespace MvcStore.Controllers
         {
             product.Id = _products.Count + 1;
             _products.Add(product);
+            return RedirectToAction("Index")    ;
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var product = _products.Find(e => e.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product Updateproduct)
+        {
+            var product = _products.Find(c => c.Id == Updateproduct.Id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            product.Id = Updateproduct.Id;
+            product.Name = Updateproduct.Name;
+            product.Price = Updateproduct.Price;
+            product.Quantity = Updateproduct.Quantity;
+
             return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = _products.Find(d => d.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult DeleteDeleteConfirmed(int id)
+        {
+            var product = _products.Find(d => d.Id == id);
+            if(product != null)
+            {
+                _products.Remove(product);
+            }
+            return RedirectToAction("index");
         }
     }   
 }
